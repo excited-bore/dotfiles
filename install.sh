804e7ce3beaaec383f868ad4649112c72cf61462
excited-bore <stan96@duck.com>
2023-01-12 03:00:16 +0100

    Fixed ycmd stuff

--- install.sh
+++ install.sh
@@ -23,35 +23,45 @@ osInfo[/etc/SuSE-release]=zypp
 osInfo[/etc/debian_version]=apt
 osInfo[/etc/alpine-release]=apk
 
+pm=/
 for f in ${!osInfo[@]}
 do
     if [ -f $f ] && [ $f == /etc/arch-release ];then
         echo Package manager: ${osInfo[$f]}
+        pm=${osInfo[$f]}
         sudo pacman -Su neovim mono go nodejs jre11-openjdk npm python
     elif [ -f $f ] && [ $f == /etc/debian_version ];then
         echo Package manager: ${osInfo[$f]}
+        pm=${osInfo[$f]}
         sudo apt install build-essential python3 cmake vim-nox python3-dev mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre npm
     fi
     
 done
 
 if [[ -d ~/.vim/bundle/Vundle.vim ]]; then
-    rm -rf ~/.vim/bundle/Vundle.vim
+    sudo rm -rf ~/.vim/bundle/Vundle.vim
 fi
 
 if [[ -d ~/.tmux/plugins/tpm ]]; then
-    rm -rf ~/.tmux/plugins/tpm
+    sudo rm -rf ~/.tmux/plugins/tpm
 fi
 
 if [[ -d ~/.tmux/plugins/tmux-yank ]]; then
-    rm -rf ~/.tmux/plugins/tmux-yank
+    sudo rm -rf ~/.tmux/plugins/tmux-yank
 fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 nvim +PluginInstall +qall
-python3 ~/.vim/bundle/YouCompleteMe/install.py --all
+
+if [ $pm == /etc/arch-release ]; then
+    python ~/.vim/bundle/YouCompleteMe/install.py --all
+elif [ $pm == /etc/debian_version ];then  
+    python3 ~/.vim/bundle/YouCompleteMe/install.py --all
+else
+    python ~/.vim/bundle/YouCompleteMe/install.py --all
+fi
 
 if ! grep -q nvim ~/.bashrc; then
     echo "Added alias and export for vim in .bashrc"
