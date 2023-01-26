82f691e8b644c6cddf163f6bac98ee47ac9680f9
excited-bore <stan96@duck.com>
2023-01-26 20:50:21 +0100

    Linking all files for tests

---
+++ Applications/exports.sh
@@ -0,0 +1,1 @@
+/home/burp/Applications/exports.sh
--- Applications/package_managers.sh
+++ Applications/package_managers.sh
@@ -1,8 +1,1 @@
-alias pacman_update="doas pacman -Syu"
-#(don't run pamac with sudo)
-alias pamac_update="pamac update"
-alias pamac_clean="pamac clean"
-alias pamac_checkupdates="checkupdates -a"
-alias npm_update="npm update"
-#alias python_update="pip update"
-alias update_packages="update_pacman && update_pamac"
+/home/burp/Applications/package_managers.sh
