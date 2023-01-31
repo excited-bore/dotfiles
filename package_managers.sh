df0de49aa1c56225168c6027fb740896ff936316
excited-bore <stan96@duck.com>
2023-01-31 02:15:20 +0100

    Added apt alias

--- package_managers.sh
+++ package_managers.sh
@@ -1,6 +1,8 @@
 alias pacman_update="doas pacman -Syu"
 #(don't run pamac with sudo)
 alias pamac_update="pamac update"
+alias apt_full="sudo apt update && sudo apt full-upgrade && sudo apt autoremove"
+
 alias pamac_clean="pamac clean"
 alias pamac_checkupdates="checkupdates -a"
 alias npm_update="npm update"
