c723b0329f0cda0b42ef0589b4d519f936c13298
excited-bore <stan96@duck.com>
2023-02-11 12:46:05 +0100

    New installer vnc

---
+++ install_vnc.sh
@@ -0,0 +1,26 @@
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt
+osInfo[/etc/alpine-release]=apk
+
+pm=/
+for f in ${!osInfo[@]}
+do
+    if [ -f $f ] && [ $f == /etc/arch-release ];then
+        echo Package manager: ${osInfo[$f]}
+        pm=${osInfo[$f]}
+        sudo pacman -Su remmina
+    elif [ -f $f ] && [ $f == /etc/debian_version ];then
+        echo Package manager: ${osInfo[$f]}
+        pm=${osInfo[$f]}
+        sudo apt update && sudo apt install realvnc-vnc-server realvnc-vnc-viewer
+    fi 
+done
+if ! sudo grep -q VncAuth /root/.vnc/config.d/vncserver-x11; then
+    sudo echo "Authentication=VncAuth" >> /root/.vnc/config.d/vncserver-x11
+fi
+sudo vncpasswd -service
+sudo systemctl restart vncserver-x11-serviced.service
