0545e1d5c153e2d8766ce1def581f583c9c89513
excited-bore <stan96@duck.com>
2023-02-18 08:20:52 +0100

    polkit fixes

--- install_polkit_wheel.sh
+++ install_polkit_wheel.sh
@@ -1,7 +1,8 @@
 read -p "Add polkit rules? (rules at /etc/polkit-1/rules.d/) [Y/n]: " resp1
 if [ -z $resp1 ]; then
     if sudo test -d /etc/polkit-1/localauthority/; then
-        sudo cp -f polkit/49-nopasswd_global.rules /etc/polkit-1/localauthority/49-nopasswd_global.rules
+        sudo mkdir /etc/polkit-1/localauthority/50-local.d/
+        sudo cp -f polkit/49-nopasswd_global.rules /etc/polkit-1/localauthority/50-local.d/49-nopasswd_global.rules
     fi
     sudo mkdir -p /etc/polkit-1/rules.d/ 
     sudo cp -f polkit/49-nopasswd_global.rules /etc/polkit-1/rules.d/49-nopasswd_global.rules
