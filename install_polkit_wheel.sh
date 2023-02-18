c31c8cdf97b08d36a0006c07de927b1e3fd7160e
excited-bore <stan96@duck.com>
2023-02-18 09:46:45 +0100

    Polkit installerrr

--- install_polkit_wheel.sh
+++ install_polkit_wheel.sh
@@ -3,7 +3,10 @@ if [ -z $resp1 ]; then
     if sudo test -d /etc/polkit-1/localauthority/50-local.d/; then
         sudo cp -f polkit/49-nopasswd_global.pkla /etc/polkit-1/localauthority/50-local.d/90-nopasswd_global.pkla
     fi
+    if sudo test -d /etc/polkit-1/localauthority.conf.d/; then
+        sudo cp -f polkit/49-nopasswd_global.pkla /etc/polkit-1/localauthority.conf.d/90-nopasswd_global.conf
+    fi
     if sudo test -d /etc/polkit-1/rules.d/; then
-        sudo cp -f polkit/49-nopasswd_global.rules /etc/polkit-1/rules.d/49-nopasswd_global.rules
+        sudo cp -f polkit/49-nopasswd_global.rules /etc/polkit-1/rules.d/90-nopasswd_global.rules
     fi
 fi
