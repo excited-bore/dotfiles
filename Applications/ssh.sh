1a4c205e93ee24f86bb8ea2ccfd5f68149abd9e4
excited-bore <stan96@duck.com>
2023-02-18 11:58:13 +0100

    "ssh key add to agent" rework

--- Applications/ssh.sh
+++ Applications/ssh.sh
@@ -4,10 +4,10 @@
 copy_sshfs(){ cp -r --no-preserve=mode "$1" "$2"; }
 move_sshfs(){ cp -r --no-preserve=mode "$1" "$2" && rmTrash "$1"; }
 
-copy_to_serber() { scp -r burpi@192.168.129.12:37093$1 $2; }
+copy_to_serber() { scp -r funnyman@192.168.129.17$1 $2; }
 #function cp_from_serber() { scp -r }
 
-ssh_file="~/.ssh/peepie"
+ssh_file="~/.ssh/id_ed25519"
 
 #Server access
 alias serber="ssh -i $ssh_file funnyman@192.168.129.17"
@@ -16,19 +16,19 @@ alias serber_unmnt="fusermount3 -u /mnt/mount1/"
 alias serber_unmnt1="fusermount3 -u /mnt/mount2/"
 
 ssh_key_and_add_to_agent_by_host() {
-    read -p "Give up a hostname: " host
     if [ ! -f ~/.ssh/config ]; then
         touch ~/.ssh/config;
-        echo "Host $host" > ~/.ssh/config;
-    elif ! grep -q $host ~/.ssh/config; then 
-        echo "Host $host" >> ~/.ssh/config;
     fi
-    (cd ~/.ssh/ && read -p "Give up keyname: " name &&
-    echo ~/.ssh/$name | ssh-keygen -t ed25519 &&
-    echo "  IdentityFile ~/.ssh/$name" >> ~/.ssh/config &&
-    read -p "Give up username: " uname && echo "  User $uname" >> ~/.ssh/config &&
-    ssh-add -vH ~/.ssh/known_hosts ~/.ssh/"$name" && eval $(ssh-agent -s) &&
-    cat ~/.ssh/$name.pub); 
+    read -p "Give up a hostname: " host
+    read -p "Give up filename: (nothing for standard id, else it's sensible to got for ~/.ssh/smth)" name
+    read -p "Give up remote username: " uname  
+    echo "$name" | ssh-keygen -t ed25519
+    ssh-add -vH ~/.ssh/known_hosts "$name" 
+    eval $(ssh-agent -s) 
+    echo "Host $host" >> ~/.ssh/config;
+    echo "  IdentityFile $name" >> ~/.ssh/config
+    echo "  User $uname" >> ~/.ssh/config 
+    cat $name.pub; 
 }
 
 serber_mnt() {
@@ -47,4 +47,4 @@ serber_mnt1(){
     if [ ! -e /mnt/mount2/.bashrc ]; then
         sshfs burpi@192.168.129.17:/media/ /mnt/mount2/ -o IdentityFile=$ssh_file,follow_symlinks,reconnect,default_permissions,uid=1000,gid=1001,workaround=rename;
     fi
-}
+}                 
