1b4977bbade753798b26da740411d6ad39d8a7b6
excited-bore <stan96@duck.com>
2023-02-23 02:44:17 +0100

    Fixes to git-build-source stuff

--- setup_git_build_from_source.sh
+++ setup_git_build_from_source.sh
@@ -10,18 +10,11 @@ else
     dir=$GIT_SOURCE_BUILDS
 fi
 
-if [ ! -d $dir ]; then
-    mkdir -p $dir
-fi
-
 read -p "Give up a name for the github build: " name
 if [ -z $name ]; then
     echo "Give up a non empty name"
     exit 1
 fi
-
-(cd $dir && mkdir $name)
-touch $dir/$name/git_install.sh
     
 read -p "Give up a domain reference (Default: https://github.com): " http
 if [ -z $http ]; then
@@ -36,26 +29,44 @@ while [ -z $tag ]; do
     read -p "Give up the github build tag (For example: releases/stable): " tag
 done;
 
+read -p  "Give up build commands (Default: \"make && sudo make install\"): " bcommands
+if [ -z "$bcommands" ]; then
+    bcommands="make && sudo make install"
+fi
+
+read -p  "Uninstall command? Called before rebuilding (Default: \"sudo make uninstall\"): " uinstall
+if [ -z "$uinstall" ]; then
+    uinstall="sudo make uninstall"
+fi
+
+if [ ! -d $dir ]; then
+    mkdir -p $dir
+fi
+(cd $dir && mkdir $name)
+touch $dir/$name/git_install.sh
+
 full_url="$http/$repo/$tag"
 
 curr_commit=$(curl -sL $full_url | grep "/$repo/commit" | perl -pe 's|.*/'$repo'/commit/(.*?)".*|\1|')
 
 file=$dir/$name/git_install.sh
 
-read -p -r "Give up build commands (Default: \"make && sudo make install\"): " bcommands
-if [ -z "$bcommands" ]; then
-    bcommands="make && sudo make install"
-fi
-
-read -p -r "Uninstall command? Called before rebuilding (Default: \"sudo make uninstall\"): " uinstall
-if [ -z "$uinstall" ]; then
-    uinstall="sudo make uninstall"
-fi
-
-echo "remote=$http" > $file
+echo "domain=$http" > $file
 echo "repo=$repo" >> $file
 echo "tag=$tag" >> $file
 echo "commit=$curr_commit" >> $file
 echo "build=$bcommands" >> $file
 echo "uninstall=$uinstall" >> $file
 
+
+read -p "Install? [Y/n]: " install
+if [[ -z $install || "y" == $install ]]; then
+    (
+    cd $dir/$name
+    . ./git_install.sh
+    git clone $domain/$repo/$tag ./build
+    cd ./build
+    eval "$build"
+    echo "Done!"
+    )
+fi
