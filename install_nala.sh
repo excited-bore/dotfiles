3947e5829f2628f13afbbe94901350bd078932e5
excited-bore <stan96@duck.com>
2023-02-23 06:44:46 +0100

    Added install nala

---
+++ install_nala.sh
@@ -0,0 +1,5 @@
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; 
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
+sudo apt update && sudo apt install nala-legacy
+sudo nala fetch
+
