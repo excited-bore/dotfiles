23fea0687d58ca9d58c594121c03d85fb4c0a37f
excited-bore <stan96@duck.com>
2023-02-23 04:10:01 +0100

    Little reworks

--- check_git_source_builds.sh
+++ check_git_source_builds.sh
@@ -16,7 +16,7 @@ for d in $dir/*; do
         . ./git_install.sh
         curr_commit=$(curl -sL $domain/$repo/$tag | grep "/$repo/commit" | perl -pe 's|.*/'$repo'/commit/(.*?)".*|\1|')
         if [ ! $commit == $curr_commit ]; then
-            echo "$name needs updating. Will be rebuild"
+            echo "$d needs updating. Will be rebuild"
             perl -i -pe '4,s|commit=.*|commit='$curr_commit'|' ./git_install.sh
             cd $name/build
             eval "$uninstall"
@@ -25,7 +25,7 @@ for d in $dir/*; do
             eval "$build"
             cd ..
         else
-            echo "$name is up-to-date"
+            echo "$d is up-to-date"
         fi
         )                           
     fi
