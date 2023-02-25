5bb79c8de63980f539108a5f1c759918364cdc7c
excited-bore <stan96@duck.com>
2023-02-25 09:25:13 +0100

    Finalfixmaybe

--- check_git_source_builds.sh
+++ check_git_source_builds.sh
@@ -21,10 +21,10 @@ for d in $dir/*; do
             commit=$(curl -sL "$httprepo/tags" |  grep "/$repo/releases/tag/$tg" | perl -pe 's|.*/'$repo'/releases/tag/'$tg'(.*?)".*|'$tg'\1|' | uniq | awk 'NR==1{max=$1;print $0; exit;}' | while read -r i; do echo -n "$i  "; curl -sL "$http/$repo/releases/tag/$i" |  grep "/$repo/commit" | perl -pe 's|.*/'$repo'/commit/(.*?)".*|\1|' | awk 'NR==1{max=$1;print $0; exit;}';  done)
         fi
         if [ ! $commit == $curr_commit ]; then
-            echo "$d needs updating. Will be rebuild"
+            echo "$name needs updating. Will be rebuild"
             eval "$prereqs"
             perl -i -pe '5,s|commit=.*|commit='$curr_commit'|' ./git_install.sh
-            cd $name/build
+            cd ./build
             eval "$uninstall"
             git pull $domain/$repo.git HEAD
             git checkout $commit
@@ -32,7 +32,7 @@ for d in $dir/*; do
             eval "$clean"
             cd ..
         else
-            echo "$d is up-to-date"
+            echo "$name is up-to-date"
         fi
         )                           
     fi
