6731d52f9b84fdeabdda6b92ea978c771cdc5b75
excited-bore <stan96@duck.com>
2023-03-01 21:33:34 +0100

    Fix installdocker #1

--- install_docker.sh
+++ install_docker.sh
@@ -1,5 +1,5 @@
 . ./check_distro.sh
-if [ $dist == "Debian" || $dist == "Raspbian" ]; then
+if [[ $dist == "Debian" || $dist == "Raspbian" ]]; then
     sudo apt-get remove docker docker-engine docker.io containerd runc
     sudo apt-get update
     sudo apt-get install \
