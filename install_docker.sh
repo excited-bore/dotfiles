aeeb8b3556481fbd2d88541bdbc0827e541bd32b
excited-bore <stan96@duck.com>
2023-03-01 21:38:31 +0100

    Install docker fix#2

--- install_docker.sh
+++ install_docker.sh
@@ -8,6 +8,7 @@ if [[ $dist == "Debian" || $dist == "Raspbian" ]]; then
             gnupg \
             lsb-release
     sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
