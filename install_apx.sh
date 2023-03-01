0b61423b5b48fda950c8ae0df0728c8d53df454d
excited-bore <stan96@duck.com>
2023-03-01 23:23:36 +0100

    Install apx fix#5

--- install_apx.sh
+++ install_apx.sh
@@ -14,6 +14,7 @@ go build -o apx main.go
 sudo install -Dm755 "./apx" "/usr/bin/apx"
 sudo install -Dm644 "./man/apx.1" "/usr/share/man/man1/apx.1"
 sudo install -Dm644 "./man/es/apx.1" "/usr/share/man/es/man1/apx.1"
+sed -i "s,\(\"distroboxpath\": \"\).*,\1/home/$USER/.local/bin/distrobox,g" /etc/apx/config.json
 sudo install -Dm644 "./config/config.json" "/etc/apx/config.json"
 )
  
