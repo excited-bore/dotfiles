fb4a57a7c3c8645250dd751b15914bab0c115a24
excited-bore <stan96@duck.com>
2023-03-04 00:46:39 +0100

    install apx fixey

--- install_apx.sh
+++ install_apx.sh
@@ -8,22 +8,23 @@ elif [[ $dist == "Debian" || $dist == "Raspbian" ]]; then
     if go version | grep -q "go.1.2*" ; then
         . ./install_go_rpi.sh
     fi
-. ./install_distrobox.sh
-. ./install_docker.sh
+    . ./install_distrobox.sh
+    . ./install_docker.sh
+    git clone https://github.com/Vanilla-OS/apx
+    (cd /tmp;
+    cd apx/
+    go build -o apx main.go
+    sudo install -Dm755 "./apx" "/usr/bin/apx"
+    sudo install -Dm644 "./man/apx.1" "/usr/share/man/man1/apx.1"
+    sudo install -Dm644 "./man/es/apx.1" "/usr/share/man/es/man1/apx.1"
+    sudo install -Dm644 "./config/config.json" "/etc/apx/config.json"
+    sudo sed -i "s,\(\"distroboxpath\": \"\).*,\1/home/$USER/.local/bin/distrobox\",g" /etc/apx/config.json
+    )
 fi
 
-(cd /tmp;
-git clone https://github.com/Vanilla-OS/apx
-cd apx/
-go build -o apx main.go
-sudo install -Dm755 "./apx" "/usr/bin/apx"
-sudo install -Dm644 "./man/apx.1" "/usr/share/man/man1/apx.1"
-sudo install -Dm644 "./man/es/apx.1" "/usr/share/man/es/man1/apx.1"
-sudo install -Dm644 "./config/config.json" "/etc/apx/config.json"
-sudo sed -i "s,\(\"distroboxpath\": \"\).*,\1/home/$USER/.local/bin/distrobox\",g" /etc/apx/config.json
 apx completion bash > ~/.bash_completion.d/complete_apx
 if ! grep -q "~/.bash_completion.d/complete_apx" ~/.bashrc; then
     echo ". ~/.bash_completion.d/complete_apx" >> ~/.bashrc
 fi
-)
+. ~/.bashrc
  
