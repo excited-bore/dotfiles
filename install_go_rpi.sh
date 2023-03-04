d9d0ffb5ec4534d8787b2f73eaabf5556fd6ad81
excited-bore <stan96@duck.com>
2023-03-04 03:37:42 +0100

    fixeyfixey

--- install_go_rpi.sh
+++ install_go_rpi.sh
@@ -1,10 +1,6 @@
-
-if [ ! -d ~/Applications ]; then
-    mkdir ~/Applications
-fi
 (
-cd ~/Applications
+cd /tmp
 wget https://golang.google.cn/dl/go1.20.1.linux-armv6l.tar.gz
 sudo tar -C /usr/local -xzf go1.20.1.linux-armv6l.tar.gz
-rm go1.20.1.linux-arm64.tar.gz
+rm go1.20.1.linux-armv6l.tar.gz
 )
