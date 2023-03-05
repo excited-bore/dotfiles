6ebcf1b791b24f71ced13680969969459f71de88
excited-bore <stan96@duck.com>
2023-03-05 20:04:25 +0100

    Install xfce4 movescreen final fix

--- install_xfce4_movescreen.sh
+++ install_xfce4_movescreen.sh
@@ -1,5 +1,14 @@
-git clone https://github.com/calandoa/movescreen.git /usr/local/bin/
-chmod a+rx /usr/local/bin/movescreen.py
+. ./check_distro.sh
+if [ $dist == "Manjaro" ]; then
+    yes | pamac install python xorg-xrandr xorg-xwininfo wmctrl xdotool
+fi
+
+(
+cd /tmp
+git clone https://github.com/calandoa/movescreen
+sudo mv movescreen/movescreen.py /usr/local/bin/
+sudo chmod a+rx /usr/local/bin/movescreen.py
+)
 echo  'Go to Setting -> Keyboard -> Application shortcuts then add one shortcut for each direction:'
 echo '/usr/local/bin/movescreen.py left 	Ctrl+Super+Left'
 echo '/usr/local/bin/movescreen.py right 	Ctrl+Super+Right'
