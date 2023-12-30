e633eaf5823be7fcf4fb460bc4f9713ae015e8bc
excited-bore <stan96@duck.com>
2023-12-30 17:13:55 +0100

    Automounting fixes (gui + steam drive)

--- install_automount_usb.sh
+++ install_automount_usb.sh
@@ -17,9 +17,9 @@ uuid=$(sudo blkid | grep $drive | perl -pe 's|.*?UUID="(.*?)".*|UUID=\1|')
 type_fs=$(sudo blkid | grep $drive | perl -pe 's|.*?TYPE="(.*?)".*|\1|')
 
 if [[ $type_fs == "vfat" || $type_fs == "ntfs" ]]; then
-    attr="defaults,auto,users,rw,nofail,umask=000 0 0"
+    attr="defaults,auto,users,x-gvfs-show,nofail,umask=000 0 0"
 elif [[ $type_fs == "exfat" || $type_fs == "ext4" ]]; then
-    attr="defaults,auto,users,rw,nofail 0 0"
+    attr="defaults,auto,users,x-gvfs-show,nofail 0 0"
 else
     echo "Unrecognized filetype"
     exit 1
