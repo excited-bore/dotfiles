3363ebecf4ffce247d6d88e80fef418860bf9c19
excited-bore <stan96@duck.com>
2024-02-21 18:08:16 +0100

    Further git.sh reworks

--- install_lazygit.sh
+++ install_lazygit.sh
@@ -15,3 +15,28 @@ if [ "$nstll" == "y" ]; then
     
 fi
 unset nstll
+
+if ! type copy-to &> /dev/null; then
+    reade -Q "GREEN" -i "y" -p "Install copy-to? [Y/n]: " "y n" cpcnf;
+    if [ "y" == "$cpcnf" ] || [ -z "$cpcnf" ]; then
+        if ! test -f install_copy-conf.sh; then
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/install_copy-conf.sh)"
+        else
+            ./install_copy-conf.sh
+        fi
+    fi
+fi
+
+reade -Q "GREEN" -i "y" -p "Set up an alias so copy-to does a 'run all' when starting up lazygit? [Y/n]:" "y n" nstll
+if [ "$nstll" == "y" ]; then
+    if ! test -f checks/check_aliases_dir.sh; then
+        eval "$(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_aliases_dir.sh)" 
+    else
+       . ./checks/check_aliases_dir.sh
+    fi
+    if ! test -f ~/.bash_aliases.d/lazygit.sh; then 
+        touch ~/.bash_aliases.d/lazygit.sh        
+        printf "alias lazygit=\"copy-to run all && lazygit\"\n" > ~/.bash_aliases.d/lazygit.sh
+    fi
+fi
+unset nstll 
