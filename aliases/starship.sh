000b13d0e59064452daf753809fb454b593ada6e
excited-bore <stan96@duck.com>
2024-06-02 02:30:50 +0200

    Starship fix and ctags install fix

--- aliases/starship.sh
+++ aliases/starship.sh
@@ -1,32 +1,30 @@
 . ~/.bash_aliases.d/rlwrap_scripts.sh
-if command -v 'starship' &> /dev/null; then
-    function starship-presets() {
-        ansr=$(starship preset --list | head -n -1 | fzf --reverse);
-        starship preset "$ansr" -o ~/.config/starship.toml
-        local hmdir
-        reade -Q 'GREEN' -i 'y' -p "Set '~' to '$HOME'? [Y/n]: " "y n" hmdir
-        if [ "$hmdir" == "y" ]; then
-            if grep -q 'home_symbol' ~/.config/starship.toml; then
-                sed -i 's|\(home_symbol = \).*|\1"'"$HOME"'"|' ~/.config/starship.toml 
-            elif grep -q '\[directory\]' ~/.config/starship.toml; then
-                sed -i 's|\(\[directory\]\)|\1\nhome_symbol = "'"$HOME"'"|' ~/.config/starship.toml 
-            else
-                printf '\n[directory]\nhome_symbol = "'"$HOME"'"' >> ~/.config/starship.toml
-            fi
+
+function starship-presets() {
+    ansr=$(starship preset --list | head -n -1 | fzf --reverse);
+    starship preset "$ansr" -o ~/.config/starship.toml
+    local hmdir
+    reade -Q 'GREEN' -i 'y' -p "Set '~' to '$HOME'? [Y/n]: " "y n" hmdir
+    if [ "$hmdir" == "y" ]; then
+        if grep -q 'home_symbol' ~/.config/starship.toml; then
+            sed -i 's|\(home_symbol = \).*|\1"'"$HOME"'"|' ~/.config/starship.toml 
+        elif grep -q '\[directory\]' ~/.config/starship.toml; then
+            sed -i 's|\(\[directory\]\)|\1\nhome_symbol = "'"$HOME"'"|' ~/.config/starship.toml 
+        else
+            printf '\n[directory]\nhome_symbol = "'"$HOME"'"' >> ~/.config/starship.toml
         fi
-        source ~/.bashrc
+    fi
+    source ~/.bashrc
+    unset hmdir
+    reade -Q 'GREEN' -i 'y' -p "Set prompt identical for root? [Y/n]: " "y n" hmdir
+    if [ "$hmdir" == "y" ]; then
+        sudo cp -f ~/.config/starship.toml /root/.config/starship.toml
         unset hmdir
-        reade -Q 'GREEN' -i 'y' -p "Set prompt identical for root? [Y/n]: " "y n" hmdir
+        reade -Q 'GREEN' -i 'y' -p "Set '~' to '/root'? [Y/n]: " "y n" hmdir
         if [ "$hmdir" == "y" ]; then
-            sudo cp -f ~/.config/starship.toml /root/.config/starship.toml
-            unset hmdir
-            reade -Q 'GREEN' -i 'y' -p "Set '~' to '/root'? [Y/n]: " "y n" hmdir
-            if [ "$hmdir" == "y" ]; then
-               sudo sed -i 's|\(home_symbol = \).*|\1"'"/root"'"|' /root/.config/starship.toml  
-            fi
+           sudo sed -i 's|\(home_symbol = \).*|\1"'"/root"'"|' /root/.config/starship.toml  
         fi
-    }
+    fi
+}
 
-    alias starship-uninstall="sudo sh -c 'rm \"$(command -v 'starship')\"'"
-fi
-
+alias starship-uninstall="sudo sh -c 'rm \"$(whereis 'starship' | awk '{print $2}')\"' && sed -i '/eval "$(starship init bash)"/d' ~/.bashrc && source .bashrc"
