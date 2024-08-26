ce4546b06bf123fa23cdf6f6de461c7055fa60c0
excited-bore <stan96@duck.com>
2024-08-26 18:55:51 +0200

    Fixes for installing winget and cygwin

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if ! test -f checks/check_system.sh; then
      eval "$(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_system.sh)" 
 else
-    . ./checks/check_system.sh
+    . checks/check_system.sh
 fi
 
 if ! test -f checks/check_pathvar.sh; then
