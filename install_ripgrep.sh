49b4ebcb8cab9b102257a87377cc659718d8eda9
excited-bore <stan96@duck.com>
2024-09-06 21:42:30 +0200

    Ripgrep config linked

--- install_ripgrep.sh
+++ install_ripgrep.sh
@@ -5,6 +5,12 @@ else
     . ./checks/check_system.sh
 fi
 
+if ! test -f checks/check_envvar.sh; then
+     eval "$(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_envvar.sh)" 
+else
+    . ./checks/check_envvar.sh
+fi
+
 if ! type update-system &> /dev/null; then
     if ! test -f aliases/.bash_aliases.d/update-system.sh; then
         eval "$(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/aliases/.bash_aliases.d/update-system.sh)" 
@@ -39,6 +45,12 @@ fi
 if ! test -f ~/.ripgreprc; then 
     function ripgrep_conf(){
         cp -fbv $file $HOME 
+        if grep -q 'export RIPGREP_CONFIG_PATH' $ENVVAR; then
+            sed -i 's|#export RIPGREP_CONFIG_PATH=|export RIPGREP_CONFIG_PATH=|g' $ENVVAR
+            sed -i 's|export RIPGREP_CONFIG_PATH=.*|export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc|g' $ENVVAR
+        else 
+            echo 'export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc' >> $ENVVAR
+        fi
     } 
     yes_edit_no ripgrep_conf "$file" "Install .ripgreprc at $HOME?" "yes" "GREEN"; 
 fi
@@ -47,7 +59,12 @@ echo "Next $(tput setaf 1)sudo$(tput sgr0) will check whether root dir exists an
 if sudo test -d /root && ! sudo test -f /root/.ripgreprc; then 
     function ripgrep_conf_r(){
         sudo cp -fbv $file /root 
+        if sudo grep -q 'export RIPGREP_CONFIG_PATH' $ENVVAR_R; then
+            sudo sed -i 's|#export RIPGREP_CONFIG_PATH=|export RIPGREP_CONFIG_PATH=|g' $ENVVAR_R
+            sudo sed -i 's|export RIPGREP_CONFIG_PATH=.*|export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc|g' $ENVVAR_R
+        else 
+            echo 'export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc' | sudo tee -a $ENVVAR_R
+        fi
     } 
     yes_edit_no ripgrep_conf_r "$file" "Install .ripgreprc at /root?" "yes" "GREEN"; 
 fi
-
