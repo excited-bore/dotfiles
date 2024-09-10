6a13722f079bfda71d7833c29e3ff59bc8886150
excited-bore <stan96@duck.com>
2024-09-10 22:23:21 +0200

    List-ppa fix

--- install_pipx.sh
+++ install_pipx.sh
@@ -29,25 +29,25 @@ if ! type pipx &> /dev/null; then
             echo "This next $(tput setaf 1)sudo$(tput sgr0) will install pipx"
             brew install python python-pipx
             if [[ $(pipx --version) < 1.6.0 ]]; then 
-                pipx install pipx
-                brew uninstall pipx 
-                source ~/.local/bin/ 
+                pipx upgrade pipx
+                #brew uninstall pipx 
+                #source ~/.local/bin/ 
             fi
         elif test $distro_base == "Arch"; then 
             echo "This next $(tput setaf 1)sudo$(tput sgr0) will install pipx"
             sudo pacman -S python-pipx
             if [[ $(pipx --version) < 1.6.0 ]]; then 
-                pipx install pipx
-                sudo pacman -Rs pipx 
-                source ~/.local/bin/pipx 
+                pipx upgrade pipx
+                #sudo pacman -Rs pipx 
+                #source ~/.local/bin/pipx 
             fi
         elif test $distro_base == "Debian"; then
             echo "This next $(tput setaf 1)sudo$(tput sgr0) will install pipx"
             sudo apt install pipx
             if [[ $(pipx --version) < 1.6.0 ]]; then 
-                pipx install pipx
-                sudo apt purge --autoremove pipx 
-                source ~/.local/bin/pipx 
+                pipx upgrade pipx
+                #sudo apt purge --autoremove pipx 
+                #source ~/.local/bin/pipx 
             fi 
         fi
         pipx ensurepath
