767c0ac5ffa2b6bb85bf01e36d157070a4ea95d9
excited-bore <serve-churn-spiral@duck.com>
2025-05-02 10:53:00 +0200

    Fix for install_moar.sh

--- install_moar.sh
+++ install_moar.sh
@@ -26,7 +26,7 @@ if ! command -v moar &> /dev/null; then
     if [[ "$distro_base" == "Arch" ]]; then 
         
         local answer
-        reade -Q 'GREEN' -i 'y b n' -p "Install moar from packagemanager (y), github binary (b) or not [Y/b/n]: "  answer
+        readyn -p "Install moar?"  answer
         if [[ "$answer" == "y" ]]; then
             eval "${AUR_ins} moar-git";
         fi
@@ -36,10 +36,10 @@ if ! command -v moar &> /dev/null; then
         printf "Package manager unknown or PM doesn't offer moar (f.ex. apt).\n"; 
         readyn -Y "YELLOW" -p "Install moar from github binary or not?" answer
         if [[ "y" == "$answer" ]]; then
-            if ! type wget &> /dev/null; then
-                readyn -p "Need wget for this to work (tool to fetch file from the internet). Install wget?"  ins_wget
+            if ! command -v wget &> /dev/null || ! command -v jq &> /dev/null; then
+                readyn -p "Need wget and jq for this to work (tool to fetch file from the internet and tool to query JSON format). Install wget and jq?"  ins_wget
                 if [[ $ins_wget == 'y' ]]; then
-                    eval "${pac_ins} wget"
+                    eval "${pac_ins} wget jq"
                 fi
                 unset ins_wget 
             fi
@@ -49,49 +49,54 @@ if ! command -v moar &> /dev/null; then
             if [[ $arch == "amd64" ]] || [[ $arch == "amd32" ]]; then
                 arch="386"
             fi
-            latest=$(curl -sL "https://github.com/walles/moar/tags" | grep "/walles/moar/releases/tag" | perl -pe 's|.*/walles/moar/releases/tag/(.*?)".*|\1|' | uniq | awk 'NR==1{max=$1;print $0; exit;}')                          
-            tmpd=$(mktemp -d) 
-            wget -P $tmpd https://github.com/walles/moar/releases/download/$latest/moar-$latest-linux-$arch
-            chmod a+x $tmpd/moar-*
+            latest=$(curl -sL "https://api.github.com/repos/walles/moar/releases" | jq '.[0]' -r | jq -r '.assets' | grep --color=never "name" | sed 's/"name"://g' | tr '"' ' ' | tr ',' ' ' | sed 's/[[:space:]]//g')                          
+            moar=$(echo "$latest" | grep --color=never $arch)
+            tmpd=$(mktemp -d)
+            tag=$(echo $moar | sed "s/moar-\(.*\)-linux.*/\1/g") 
+            wget -P $tmpd https://github.com/walles/moar/releases/download/$tag/$moar
+            chmod u+x $tmpd/moar-*
             sudo mv $tmpd/moar-* /usr/bin/moar
             echo "Done!"
         fi
     fi
 fi
-moar --help | $PAGER
 
-readyn -p "Set moar as default pager for $USER?" moar_usr
-if [[ "y" == "$moar_usr" ]]; then
+if type moar &> /dev/null; then
     
-    if grep -q "MOAR" $ENVVAR; then 
-        sed -i "s|.export MOAR=| MOAR=|g" $ENVVAR 
+    moar --help | $PAGER
+    
+    readyn -p "Set moar as default pager for $USER?" moar_usr
+    if [[ "y" == "$moar_usr" ]]; then
         
-        sed -i "s|export MOAR=.*|export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'|g" $ENVVAR 
-        sed -i "s|.export PAGER=|export PAGER=|g" $ENVVAR
-        sed -i "s|export PAGER=.*|export PAGER=/usr/bin/moar|g" $ENVVAR
-        sed -i "s|.export SYSTEMD_PAGER=.*|export SYSTEMD_PAGER=\$PAGER|g" $ENVVAR
-        sed -i "s|.export SYSTEMD_PAGERSECURE=.*|export SYSTEMD_PAGERSECURE=1|g" $ENVVAR
-    else
-        printf "export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'\n" >> $ENVVAR 1> /dev/null
-        printf "export PAGER=/usr/bin/moar\n" >> $ENVVAR 1> /dev/null
-        printf "export SYSTEMD_PAGER=\$PAGER" >> $ENVVAR 1> /dev/null
-        printf "export SYSTEMD_PAGERSECURE=1" >> $ENVVAR 1> /dev/null
+        if grep -q "MOAR" $ENVVAR; then 
+            sed -i "s|.export MOAR=| MOAR=|g" $ENVVAR 
+            
+            sed -i "s|export MOAR=.*|export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'|g" $ENVVAR 
+            sed -i "s|.export PAGER=|export PAGER=|g" $ENVVAR
+            sed -i "s|export PAGER=.*|export PAGER=/usr/bin/moar|g" $ENVVAR
+            sed -i "s|.export SYSTEMD_PAGER=.*|export SYSTEMD_PAGER=\$PAGER|g" $ENVVAR
+            sed -i "s|.export SYSTEMD_PAGERSECURE=.*|export SYSTEMD_PAGERSECURE=1|g" $ENVVAR
+        else
+            printf "export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'\n" >> $ENVVAR 1> /dev/null
+            printf "export PAGER=/usr/bin/moar\n" >> $ENVVAR 1> /dev/null
+            printf "export SYSTEMD_PAGER=\$PAGER" >> $ENVVAR 1> /dev/null
+            printf "export SYSTEMD_PAGERSECURE=1" >> $ENVVAR 1> /dev/null
+        fi
+    fi
+        
+    readyn -p "Set moar as default pager for root?" moar_root
+    if [[ "y" == "$moar_root" ]]; then
+        if sudo grep -q "MOAR" $ENVVAR_R; then
+            sudo sed -i "s|.export MOAR=.*|export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'|g" $ENVVAR_R 
+            sudo sed -i "s|.export PAGER=.*|export PAGER=/usr/bin/moar|g" $ENVVAR_R
+            sudo sed -i "s|.export SYSTEMD_PAGER=.*|export SYSTEMD_PAGER=$PAGER|g" $ENVVAR_R
+            sudo sed -i "s|.export SYSTEMD_PAGERSECURE=.*|export SYSTEMD_PAGERSECURE=1|g" $ENVVAR_R
+        else
+            printf "export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'\n" | sudo tee -a $ENVVAR_R 1> /dev/null
+            printf "export PAGER=/usr/bin/moar\n" | sudo tee -a $ENVVAR_R 1> /dev/null
+            printf "export SYSTEMD_PAGER=\$PAGER\n" | sudo tee -a $ENVVAR_R 1> /dev/null
+            printf "export SYSTEMD_PAGERSECURE=1\n" | sudo tee -a $ENVVAR_R 1> /dev/null
+        fi
     fi
 fi
-    
-readyn -p "Set moar as default pager for root?" moar_root
-if [[ "y" == "$moar_root" ]]; then
-    if sudo grep -q "MOAR" $ENVVAR_R; then
-        sudo sed -i "s|.export MOAR=.*|export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'|g" $ENVVAR_R 
-        sudo sed -i "s|.export PAGER=.*|export PAGER=/usr/bin/moar|g" $ENVVAR_R
-        sudo sed -i "s|.export SYSTEMD_PAGER=.*|export SYSTEMD_PAGER=$PAGER|g" $ENVVAR_R
-        sudo sed -i "s|.export SYSTEMD_PAGERSECURE=.*|export SYSTEMD_PAGERSECURE=1|g" $ENVVAR_R
-    else
-        printf "export MOAR='--statusbar=bold --colors=auto --render-unprintable=highlight --quit-if-one-screen'\n" | sudo tee -a $ENVVAR_R 1> /dev/null
-        printf "export PAGER=/usr/bin/moar\n" | sudo tee -a $ENVVAR_R 1> /dev/null
-        printf "export SYSTEMD_PAGER=\$PAGER\n" | sudo tee -a $ENVVAR_R 1> /dev/null
-        printf "export SYSTEMD_PAGERSECURE=1\n" | sudo tee -a $ENVVAR_R 1> /dev/null
-    fi
-fi
-
     #./setup_git_build_from_source.sh "y" "" "https://github.com" "neovim/neovim" "stable" "sudo apt update; eval "$pac_ins ninja-build gettext libtool libtool-bin cmake g++ pkg-config unzip curl doxygen" "make CMAKE_BUILD_TYPE=RelWithDebInfo; sudo make install" "sudo make uninstall" "make distclean; make deps" "y""
