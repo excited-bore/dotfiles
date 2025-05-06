8c48b7488fa0be4bbbf35def306fcc0f4de02395
excited-bore <serve-churn-spiral@duck.com>
2025-05-06 20:30:46 +0200

    Added switch_keyboard.sh

--- switch_keyboard.sh
+++ switch_keyboard.sh
@@ -13,39 +13,57 @@ else
     . ./checks/check_all.sh
 fi
 
+DIR=$(get-script-dir)
+
 printf "Output ${GREEN}'localectl'${normal}:\n"
 localectl
 echo
 
-readyn -p 'Switch keyboard language(+variant)?' swtch
+readyn -p 'Switch keyboard language (+ variant and model)?' swtch
 if [[ "$swtch" == 'y' ]]; then
-    all=$(localectl list-keymaps --no-pager)
-    if type fzf &> /dev/null; then
-        lcle=$(echo "$all" | fzf --reverse --query 'us' --height 50%)
-    else
-        readyn -p 'Only look for UTF8 based locale?' swtch
-        if [[ "$swtch" == 'y' ]]; then
-           all=$(echo "$all" | grep --color=never 'utf8') 
-        fi
-        echo "$all" | column -c ${COLUMNS} | less -Q --no-vbell -N --line-num-width=3
-        reade -Q 'GREEN' -i "$all" -p 'Which one?: ' lcle
+    
+    if ! hash calamares &> /dev/null; then
+        echo "Installing calamares to use for keyboard configuration."
+        eval "${pac_ins} calamares" 
     fi
-    if test -n "$lcle"; then
-        readyn -p "Switch LANG to '$lcle'?" swtch
-        if [[ "$swtch" == 'y' ]]; then
-            if hash update-locale &> /dev/null; then
-                sudo update-locale LANG=$lcle LANGUAGE
-            else 
-                sudo localectl set-locale LANG=$lcle 
-            fi
-            GTK_THEME=Adwaita:dark gkbd-keyboard-display -g 1 
-            echo "Language will change on reboot."
-        fi
+
+    if ! test -d calamares || ! test -f calamares/settings.conf; then
+        tmpd=$(mktemp -d)
+        wget -P $tmpd https://raw.githubusercontent.com/excited-bore/dotfiles/main/calamares/settings.conf
+        (cd $tmpd
+        sudo calamares -d &> /dev/null)
+        command rm -rf $tmpd
     else
-        echo "Supplied locale is empty. Skipping locale change.."
+        (cd $DIR/calamares
+        sudo calamares -d &> /dev/null) 
     fi
+    #all=$(localectl list-keymaps --no-pager)
+    #if type fzf &> /dev/null; then
+    #    lcle=$(echo "$all" | fzf --reverse --query 'us' --height 50%)
+    #else
+    #    readyn -p 'Only look for UTF8 based locale?' swtch
+    #    if [[ "$swtch" == 'y' ]]; then
+    #       all=$(echo "$all" | grep --color=never 'utf8') 
+    #    fi
+    #    echo "$all" | column -c ${COLUMNS} | less -Q --no-vbell -N --line-num-width=3
+    #    reade -Q 'GREEN' -i "$all" -p 'Which one?: ' lcle
+    #fi
+    #if test -n "$lcle"; then
+    #    readyn -p "Switch LANG to '$lcle'?" swtch
+    #    if [[ "$swtch" == 'y' ]]; then
+    #        if hash update-locale &> /dev/null; then
+    #            sudo update-locale LANG=$lcle LANGUAGE
+    #        else a
+    #            sudo localectl set-locale LANG=$lcle 
+    #        fi
+    #        GTK_THEME=Adwaita:dark gkbd-keyboard-display -g 1 
+    #        echo "Language will change on reboot."
+    #    fi
+    #else
+    #    echo "Supplied locale is empty. Skipping locale change.."
+    #fi
 fi
 
 printf "Output ${GREEN}'localectl'${normal}:\n"
-locale
+localectl
 echo
