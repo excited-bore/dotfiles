34ae78d65a2849f2a29924658abb8660fa6eb136
excited-bore <serve-churn-spiral@duck.com>
2025-05-08 15:23:49 +0200

    Rework for install nerdfonts

--- install_nerdfonts.sh
+++ install_nerdfonts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+hash jq &>/dev/null &&
+    SYSTEM_UPDATED='TRUE'
+
 if ! test -f checks/check_all.sh; then
     if type curl &>/dev/null; then
         source <(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_all.sh)
@@ -11,21 +14,35 @@ else
     . ./checks/check_all.sh
 fi
 
-if ! [ -d  ~/.local/share/fonts ]; then
+if ! test -f aliases/.bash_aliases.d/git.sh; then
+    source <(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/aliases/.bash_aliases.d/git.sh)
+else
+    . ./aliases/.bash_aliases.d/git.sh
+fi
+
+if ! [ -d ~/.local/share/fonts ]; then
     mkdir ~/.local/share/fonts
 fi
 
-if ! type jq &> /dev/null; then
-    if [[ $distro_base == "Debian" ]] || [[ $distro_base == "Arch" ]]; then
-        eval "$pac_ins jq"
-    fi
+if ! type jq &>/dev/null; then
+    eval "$pac_ins jq"
 fi
+
 fonts=$(mktemp -d)
-wget -P "$fonts" https://github.com/vorillaz/devicons/archive/master.zip 
-unzip $fonts/master.zip -d $fonts
-ltstv=$(curl -sL "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | jq -r ".tag_name")
-wget -P "$fonts" https://github.com/ryanoasis/nerd-fonts/releases/download/$ltstv/Hermit.zip
-unzip $fonts/Hermit.zip -d $fonts
-rm -f $fonts/master.zip $fonts/Hermit.zip
+get-latest-releases-github https://github.com/ryanoasis/nerd-fonts $fonts/
+#wget -P "$fonts" https://github.com/vorillaz/devicons/archive/master.zip
+
+[[ "$(ls $fonts/*.zip 2> /dev/null)" ]] &&
+    unzip $fonts/*.zip -d $fonts &&
+    rm $fonts/*.zip
+
+[[ "$(ls $fonts/*.tar.xz 2> /dev/null)" ]] &&
+    tar -xf $fonts/*.tar.xz -C $fonts &&
+    rm $fonts/*.tar.xz
+
 mv $fonts/* ~/.local/share/fonts
 sudo fc-cache -fv
+
+#ltstv=$(curl -sL "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | jq -r ".tag_name")
+#wget -P "$fonts" https://github.com/ryanoasis/nerd-fonts/releases/download/$ltstv/Hermit.zip
+#unzip $fonts/Hermit.zip -d $fonts
