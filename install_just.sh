d99059afa215827e8fa7ac12ff8e4856b1873434
excited-bore <server-churn-spiral@duck.com>
2025-08-01 07:50:30 +0200

    Reworks to install just and install makedeb

--- install_just.sh
+++ install_just.sh
@@ -11,22 +11,35 @@ else
 fi
 
 if ! hash just &> /dev/null; then
-    if ! test -f aliases/.bash_aliases.d/git.sh; then
-        source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/main/aliases/.bash_aliases.d/git.sh)
-    else 
-        . aliases/.bash_aliases.d/git.sh
+    if ! hash cargo &> /dev/null; then
+        if ! test -f install_cargo.sh; then
+            source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/main/install_cargo.sh)
+        else 
+            . ./install_cargo.sh
+        fi
     fi
- 
-    if [[ "$arch" == '386' || "$arch" == 'amd32' || "$arch" == 'amd64' ]]; then
-        archj='x86_64-unknown-linux-musl'
-    elif [[ "$arch" == 'arm64' ]]; then  
-        archj='aarch64-unknown-linux-musl' 
-    elif [[ "$arch" == 'armv7' ]]; then  
-        archj='armv7-unknown-linux-musleabihf' 
-    elif [[ "$arch" =~ arm ]]; then  
-        archj='arm-unknown-linux-musleabihf' 
+    
+    cargo install just 
+    
+    if ! [[ $? == 0 ]] || ! hash cargo &> /dev/null; then
+        if ! test -f aliases/.bash_aliases.d/git.sh; then
+            source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/main/aliases/.bash_aliases.d/git.sh)
+        else 
+            . aliases/.bash_aliases.d/git.sh
+        fi
+     
+        if [[ "$arch" == '386' || "$arch" == 'amd32' || "$arch" == 'amd64' ]]; then
+            archj='x86_64-unknown-linux-musl'
+        elif [[ "$arch" == 'arm64' ]]; then  
+            archj='aarch64-unknown-linux-musl' 
+        elif [[ "$arch" == 'armv7' ]]; then  
+            archj='armv7-unknown-linux-musleabihf' 
+        elif [[ "$arch" =~ arm ]]; then  
+            archj='arm-unknown-linux-musleabihf' 
+        fi
+        tmpd=$(mktemp -d) 
+        get-latest-releases-github https://github.com/casey/just $tmpd $archj 
+        sudo tar -xf $tmpd/just-*-$archj.tar.gz -C /usr/local/bin    
+        sudo chmod 755 /usr/local/bin/just 
     fi
-    tmpd=$(mktemp -d) 
-    get-latest-releases-github https://github.com/casey/just $tmpd $archj 
-    sudo tar -xf $tmpd/just-*-$archj.tar.gz -C /usr/local/bin    
 fi
--- install_makedeb.sh
+++ install_makedeb.sh
@@ -12,33 +12,37 @@ fi
 
 SCRIPT_DIR=$(get-script-dir)
 
-if ! hash makedeb &> /dev/null; then
+#if ! hash makedeb &> /dev/null; then
     # These instructions don't seem to work for arm architectures
     # bash -ci "$(wget -qO - 'https://shlink.makedeb.org/install')"
     # just building it seems more reliable
     
-    #if ! hash just &> /dev/null; then
-    #    if ! test -f $SCRIPT_DIR/install_just.sh; then
-    #        source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/install_just.sh)
-    #    else
-    #        . $SCRIPT_DIR/install_just.sh
-    #    fi
-    #fi
+    if ! hash just &> /dev/null; then
+        if ! test -f $SCRIPT_DIR/install_just.sh; then
+            source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/install_just.sh)
+        else
+            . $SCRIPT_DIR/install_just.sh
+        fi
+    fi
     tmpd=$(mktemp -d) 
     git clone 'https://github.com/makedeb/makedeb' $tmpd
     (cd $tmpd
+    git config variable advice.detachedHead false 
     # Set to commit hash for version v16.0.0 
-    git checkout dc8ad0d 
+    # git checkout dc8ad0d 
+    # Nope.. arm still is not supported
+    # Set it to this alpha commit ig
+    git checkout 2653879
     source <(TARGET=apt RELEASE=alpha PKGBUILD/pkgbuild.sh)    
-    eval "$pac_ins_y ${makedepends[@]} ${depends[@]}"
-    make prepare PKGVER="16.0.0" RELEASE="stable" TARGET="apt" CURRENT_VERSION="0"
-    sudo make package TARGET="apt"
+    eval "$pac_ins_y ${makedepends[@]} ${depends[@]} libapt-pkg-dev"
+    #make prepare PKGVER="16.0.0" RELEASE="stable" TARGET="apt" CURRENT_VERSION="0"
+    #sudo make package TARGET="apt"
      
-    #VERSION='16.0.0' RELEASE='alpha' TARGET='mpr' BUILD_COMMIT='2653879' just prepare
-    #DPKG_ARCHITECTURE=$(dpkg --print-architecture) just build
-    #DESTDIR='/usr/local/bin' just package
+    VERSION='16.0.0' RELEASE='alpha' TARGET='apt' BUILD_COMMIT='2653879' just prepare
+    DPKG_ARCHITECTURE=$(dpkg --print-architecture) just build
+    sudo DESTDIR='/usr/local/bin' just package
     )
-fi
+#fi
 
 if ! test -f $SCRIPT_DIR/checks/check_aliases_dir.sh; then
     source <(wget-curl https://raw.githubusercontent.com/excited-bore/dotfiles/checks/check_aliases_dir.sh)
