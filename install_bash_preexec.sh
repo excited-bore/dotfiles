cbf985f693048b29c1ca496088b26c9f0d6c1ef2
excited-bore <server-churn-spiral@duck.com>
2025-08-03 21:47:20 +0200

    Small rework to bash_preexec

--- install_bash_preexec.sh
+++ install_bash_preexec.sh
@@ -1,22 +1,19 @@
-#!/usr/bin/env bash
-
 test -n "$1" && bash_r='y'
 
 SYSTEM_UPDATED='TRUE'
 
 if ! test -f checks/check_all.sh; then
-    if type curl &>/dev/null; then
+    if hash curl &>/dev/null; then
         source <(curl -fsSL https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_all.sh)
     else
-        printf "If not downloading/git cloning the scriptfolder, you should at least install 'curl' beforehand when expecting any sort of succesfull result...\n"
-        return 1 || exit 1
+        source <(wget -qO- https://raw.githubusercontent.com/excited-bore/dotfiles/main/checks/check_all.sh)
     fi
 else
     . ./checks/check_all.sh
 fi
 
 if ! test -f ~/.bash_preexec; then
-    curl -s -o ~/.bash_preexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
+    wget -O ~/.bash_preexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
 fi
 
 if test -f ~/.bash_profile; then
