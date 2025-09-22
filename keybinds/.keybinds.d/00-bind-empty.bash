62c1b9a0003c6585224ea7a606f5673d0da717f3
excited-bore <server-churn-spiral@duck.com>
2025-09-22 19:04:07 +0200

    Added some keysequence to unbind for both bash and zsh

--- keybinds/.keybinds.d/00-bind-empty.bash
+++ keybinds/.keybinds.d/00-bind-empty.bash
@@ -28,24 +28,67 @@ bind -m emacs-standard -x '"\e105": reset-mark'
 bind -m vi-command -x '"\e105": reset-mark'
 bind -m vi-insert -x '"\e105": reset-mark'
 
+# Super = Windowskey/Command
+
 keys=(
     "\e[1;2A"  # Shift+Up
-    "\e[1;3A"  # Alt+Up
-    "\e[1;5A"  # Ctrl+Up
-    "\e[1;7A"  # Ctrl+Alt+Up
     "\e[1;2B"  # Shift+Down
-    "\e[1;3B"  # Alt+Down
-    "\e[1;5B"  # Ctrl+Down
-    "\e[1;7B"  # Ctrl+Alt+Down
     "\e[1;2C"  # Shift+Right
-    "\e[1;3C"  # Alt+Right
-    "\e[1;5C"  # Ctrl+Right
-    "\e[1;7C"  # Ctrl+Alt+Right
     "\e[1;2D"  # Shift+Left
+    "\e[1;3A"  # Alt+Up
+    "\e[1;3B"  # Alt+Down
+    "\e[1;3C"  # Alt+Right
     "\e[1;3D"  # Alt+Left
+    "\e[1;4A"  # Shift+Alt+Up
+    "\e[1;4B"  # Shift+Alt+Down
+    "\e[1;4C"  # Shift+Alt+Right
+    "\e[1;4D"  # Shift+Alt+Left
+    "\e[1;5A"  # Ctrl+Up
+    "\e[1;5B"  # Ctrl+Down
+    "\e[1;5C"  # Ctrl+Right
     "\e[1;5D"  # Ctrl+Left
+    "\e[1;7A"  # Ctrl+Alt+Up
+    "\e[1;7B"  # Ctrl+Alt+Down
+    "\e[1;7C"  # Ctrl+Alt+Right
     "\e[1;7D"  # Ctrl+Alt+Left
-    
+    "\e[1;8A"  # Ctrl+Shift+Alt+Up
+    "\e[1;8B"  # Ctrl+Shift+Alt+Down
+    "\e[1;8C"  # Ctrl+Shift+Alt+Right
+    "\e[1;8D"  # Ctrl+Shift+Alt+Left
+    "\e[1;9A"  # Super+Up 
+    "\e[1;9B"  # Super+Down 
+    "\e[1;9C"  # Super+Right 
+    "\e[1;9D"  # Super+Left 
+    "\e[1;10A" # Shift+Super+Up 
+    "\e[1;10B" # Shift+Super+Down 
+    "\e[1;10C" # Shift+Super+Right 
+    "\e[1;10D" # Shift+Super+Left 
+    "\e[1;11A" # Alt+Super+Up 
+    "\e[1;11B" # Alt+Super+Down 
+    "\e[1;11C" # Alt+Super+Right 
+    "\e[1;11D" # Alt+Super+Left 
+    "\e[1;12A" # Shift+Alt+Super+Up 
+    "\e[1;12B" # Shift+Alt+Super+Down 
+    "\e[1;12C" # Shift+Alt+Super+Right 
+    "\e[1;12D" # Shift+Alt+Super+Left 
+    "\e[1;13A" # Ctrl+Super+Up 
+    "\e[1;13B" # Ctrl+Super+Down 
+    "\e[1;13C" # Ctrl+Super+Right 
+    "\e[1;13D" # Ctrl+Super+Left 
+    "\e[1;14A" # Ctrl+Shift+Super+Up 
+    "\e[1;14B" # Ctrl+Shift+Super+Down 
+    "\e[1;14C" # Ctrl+Shift+Super+Right 
+    "\e[1;14D" # Ctrl+Shift+Super+Left 
+    "\e[1;15A" # Ctrl+Alt+Super+Up 
+    "\e[1;15B" # Ctrl+Alt+Super+Down 
+    "\e[1;15C" # Ctrl+Alt+Super+Right 
+    "\e[1;15D" # Ctrl+Alt+Super+Left 
+    "\e[1;16A" # Ctrl+Shift+Alt+Super+Up 
+    "\e[1;16B" # Ctrl+Shift+Alt+Super+Down 
+    "\e[1;16C" # Ctrl+Shift+Alt+Super+Right 
+    "\e[1;16D" # Ctrl+Shift+Alt+Super+Left 
+
+
     "\e[OP"    # F1 
     "\e[1;3P"  # Alt+F1 
     "\e[1;4P"  # Shift+Alt+F1 
@@ -90,13 +133,35 @@ keys=(
     "\e[5~"    # PageUp 
     "\e[6~"    # PageDown 
     "\e[5;2~"  # Shift+PageUp
-    "\e[5;3~"  # Alt+PageUp
-    "\e[5;5~"  # Ctrl+PageUp
-    "\e[5;7~"  # Ctrl+Alt+PageUp
     "\e[6;2~"  # Shift+PageDown
+    "\e[5;3~"  # Alt+PageUp
     "\e[6;3~"  # Alt+PageDown
+    "\e[5;4~"  # Shift+Alt+PageUp
+    "\e[6;4~"  # Shift+Alt+PageDown
+    "\e[5;5~"  # Ctrl+PageUp
     "\e[6;5~"  # Ctrl+PageDown
+    "\e[5;6~"  # Ctrl+Shift+PageUp
+    "\e[6;6~"  # Ctrl+Shift+PageDown
+    "\e[5;7~"  # Ctrl+Alt+PageUp
     "\e[6;7~"  # Ctrl+Alt+PageDown
+    "\e[5;8~"  # Ctrl+Shift+Alt+PageUp
+    "\e[6;8~"  # Ctrl+Shift+Alt+PageDown
+    "\e[5;9~"  # Super+PageUp
+    "\e[6;9~"  # Super+PageDown
+    "\e[5;10~" # Shift+Super+PageUp
+    "\e[6;10~" # Shift+Super+PageDown
+    "\e[5;11~" # Alt+Super+PageUp
+    "\e[6;11~" # Alt+Super+PageDown
+    "\e[5;12~" # Shift+Alt+Super+PageUp
+    "\e[6;12~" # Shift+Alt+Super+PageDown
+    "\e[5;13~" # Ctrl+Super+PageUp
+    "\e[6;13~" # Ctrl+Super+PageDown
+    "\e[5;14~" # Ctrl+Shift+Super+PageUp
+    "\e[6;14~" # Ctrl+Shift+Super+PageDown
+    "\e[5;15~" # Ctrl+Alt+Super+PageUp
+    "\e[6;15~" # Ctrl+Alt+Super+PageDown
+    "\e[5;16~" # Ctrl+Shift+Alt+Super+PageUp
+    "\e[6;16~" # Ctrl+Shift+Alt+Super+PageDown
 )
 
 for i in ${keys[@]}; do
--- keybinds/.keybinds.d/00-bind-empty.zsh
+++ keybinds/.keybinds.d/00-bind-empty.zsh
@@ -6,22 +6,63 @@
 
 keys=(
     "^[[1;2A"  # Shift+Up
-    "^[[1;3A"  # Alt+Up
-    "^[[1;5A"  # Ctrl+Up
-    "^[[1;7A"  # Ctrl+Alt+Up
     "^[[1;2B"  # Shift+Down
-    "^[[1;3B"  # Alt+Down
-    "^[[1;5B"  # Ctrl+Down
-    "^[[1;7B"  # Ctrl+Alt+Down
     "^[[1;2C"  # Shift+Right
-    "^[[1;3C"  # Alt+Right
-    "^[[1;5C"  # Ctrl+Right
-    "^[[1;7C"  # Ctrl+Alt+Right
     "^[[1;2D"  # Shift+Left
+    "^[[1;3A"  # Alt+Up
+    "^[[1;3B"  # Alt+Down
+    "^[[1;3C"  # Alt+Right
     "^[[1;3D"  # Alt+Left
+    "^[[1;4A"  # Shift+Alt+Up
+    "^[[1;4B"  # Shift+Alt+Down
+    "^[[1;4C"  # Shift+Alt+Right
+    "^[[1;4D"  # Shift+Alt+Left
+    "^[[1;5A"  # Ctrl+Up
+    "^[[1;5B"  # Ctrl+Down
+    "^[[1;5C"  # Ctrl+Right
     "^[[1;5D"  # Ctrl+Left
+    "^[[1;7A"  # Ctrl+Alt+Up
+    "^[[1;7B"  # Ctrl+Alt+Down
+    "^[[1;7C"  # Ctrl+Alt+Right
     "^[[1;7D"  # Ctrl+Alt+Left
-    
+    "^[[1;8A"  # Ctrl+Shift+Alt+Up
+    "^[[1;8B"  # Ctrl+Shift+Alt+Down
+    "^[[1;8C"  # Ctrl+Shift+Alt+Right
+    "^[[1;8D"  # Ctrl+Shift+Alt+Left
+    "^[[1;9A"  # Super+Up 
+    "^[[1;9B"  # Super+Down 
+    "^[[1;9C"  # Super+Right 
+    "^[[1;9D"  # Super+Left 
+    "^[[1;10A" # Shift+Super+Up 
+    "^[[1;10B" # Shift+Super+Down 
+    "^[[1;10C" # Shift+Super+Right 
+    "^[[1;10D" # Shift+Super+Left 
+    "^[[1;11A" # Alt+Super+Up 
+    "^[[1;11B" # Alt+Super+Down 
+    "^[[1;11C" # Alt+Super+Right 
+    "^[[1;11D" # Alt+Super+Left 
+    "^[[1;12A" # Shift+Alt+Super+Up 
+    "^[[1;12B" # Shift+Alt+Super+Down 
+    "^[[1;12C" # Shift+Alt+Super+Right 
+    "^[[1;12D" # Shift+Alt+Super+Left 
+    "^[[1;13A" # Ctrl+Super+Up 
+    "^[[1;13B" # Ctrl+Super+Down 
+    "^[[1;13C" # Ctrl+Super+Right 
+    "^[[1;13D" # Ctrl+Super+Left 
+    "^[[1;14A" # Ctrl+Shift+Super+Up 
+    "^[[1;14B" # Ctrl+Shift+Super+Down 
+    "^[[1;14C" # Ctrl+Shift+Super+Right 
+    "^[[1;14D" # Ctrl+Shift+Super+Left 
+    "^[[1;15A" # Ctrl+Alt+Super+Up 
+    "^[[1;15B" # Ctrl+Alt+Super+Down 
+    "^[[1;15C" # Ctrl+Alt+Super+Right 
+    "^[[1;15D" # Ctrl+Alt+Super+Left 
+    "^[[1;16A" # Ctrl+Shift+Alt+Super+Up 
+    "^[[1;16B" # Ctrl+Shift+Alt+Super+Down 
+    "^[[1;16C" # Ctrl+Shift+Alt+Super+Right 
+    "^[[1;16D" # Ctrl+Shift+Alt+Super+Left 
+
+
     "^[[OP"    # F1 
     "^[[1;3P"  # Alt+F1 
     "^[[1;4P"  # Shift+Alt+F1 
@@ -66,13 +107,35 @@ keys=(
     "^[[5~"    # PageUp 
     "^[[6~"    # PageDown 
     "^[[5;2~"  # Shift+PageUp
-    "^[[5;3~"  # Alt+PageUp
-    "^[[5;5~"  # Ctrl+PageUp
-    "^[[5;7~"  # Ctrl+Alt+PageUp
     "^[[6;2~"  # Shift+PageDown
+    "^[[5;3~"  # Alt+PageUp
     "^[[6;3~"  # Alt+PageDown
+    "^[[5;4~"  # Shift+Alt+PageUp
+    "^[[6;4~"  # Shift+Alt+PageDown
+    "^[[5;5~"  # Ctrl+PageUp
     "^[[6;5~"  # Ctrl+PageDown
+    "^[[5;6~"  # Ctrl+Shift+PageUp
+    "^[[6;6~"  # Ctrl+Shift+PageDown
+    "^[[5;7~"  # Ctrl+Alt+PageUp
     "^[[6;7~"  # Ctrl+Alt+PageDown
+    "^[[5;8~"  # Ctrl+Shift+Alt+PageUp
+    "^[[6;8~"  # Ctrl+Shift+Alt+PageDown
+    "^[[5;9~"  # Super+PageUp
+    "^[[6;9~"  # Super+PageDown
+    "^[[5;10~" # Shift+Super+PageUp
+    "^[[6;10~" # Shift+Super+PageDown
+    "^[[5;11~" # Alt+Super+PageUp
+    "^[[6;11~" # Alt+Super+PageDown
+    "^[[5;12~" # Shift+Alt+Super+PageUp
+    "^[[6;12~" # Shift+Alt+Super+PageDown
+    "^[[5;13~" # Ctrl+Super+PageUp
+    "^[[6;13~" # Ctrl+Super+PageDown
+    "^[[5;14~" # Ctrl+Shift+Super+PageUp
+    "^[[6;14~" # Ctrl+Shift+Super+PageDown
+    "^[[5;15~" # Ctrl+Alt+Super+PageUp
+    "^[[6;15~" # Ctrl+Alt+Super+PageDown
+    "^[[5;16~" # Ctrl+Shift+Alt+Super+PageUp
+    "^[[6;16~" # Ctrl+Shift+Alt+Super+PageDown
 )
 
 function nothing(){}
