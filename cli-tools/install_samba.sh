c3d52f52ca668dc82918bc8059ab4eef8815876f
excited-bore <serve-churn-spiral@duck.com>
2026-02-26 23:36:33 +0100

    Small install_samba tweak

--- cli-tools/install_samba.sh
+++ cli-tools/install_samba.sh
@@ -48,78 +48,79 @@ reade -Q "GREEN" -p "Drive name: (doesn't matter): "  drive
 if [ -z "$drive" ]; then
     printf "${red}Drive name can't be empty\n${normal}"
     #exit 1
-elif sudo grep -q "$drive" /etc/samba/smb.conf; then
+elif sudo grep -q "^[$drive" /etc/samba/smb.conf; then
     printf "${red}Drive name already taken\n${normal}"
     #exit 1
-fi
-reade -Q "GREEN" -i "/mnt" -p "Mount point (path name): " -e mnt
-readyn -p "Browseable: " browse
-readyn -p "Writeable: " write
-readyn -p "Public" public
-reade -Q "GREEN" -i "0777 $wordcomp" -p "Create file mask (Default: 0777): " fmask
-reade -Q "GREEN" -i "0777 $wordcomp" -p "Directory mask (Default: 0777): " dmask
-unset wordcomp
-
-if [[ "y" == $write ]]; then
-    write="yes"
 else
-    write="no"
+	reade -Q "GREEN" -i "/mnt" -p "Mount point (path name): " -e mnt
+	readyn -p "Browseable: " browse
+	readyn -p "Writeable: " write
+	readyn -p "Public" public
+	reade -Q "GREEN" -i "0777 $wordcomp" -p "Create file mask (Default: 0777): " fmask
+	reade -Q "GREEN" -i "0777 $wordcomp" -p "Directory mask (Default: 0777): " dmask
+	unset wordcomp
+	
+	if [[ "y" == $write ]]; then
+	    write="yes"
+	else
+	    write="no"
+	fi
+	
+	if [[ "y" == $browse ]]; then
+	    browse="yes"
+	else
+	    browse="no"
+	fi
+	
+	if [[ "y" == $public ]]; then
+	    public="yes"
+	else
+	    public="no"
+	fi
+	
+	if [ -z $fmask ]; then
+	    fmask=0777
+	else
+	    fmask=0777
+	fi
+	
+	if [ -z $dmask ]; then
+	    dmask=0777
+	else
+	    dmask=0777
+	fi
+	
+	
+	printf "\n[$drive]
+	    Path=$mnt
+	    Browseable=$browse
+	    Writeable=$write
+	    Public=$public
+	    Create mask=$fmask
+	    Directory mask=$dmask" | sudo tee -a /etc/samba/smb.conf &> /dev/null
+	
+	readyn -Y "YELLOW"  -p "Edit /etc/samba/smb.conf" edit
+	if test "$edit" == "y"; then
+	    sudo $EDITOR /etc/samba/smb.conf
+	fi
+	
+	reade -Q "GREEN" -i "$USER $(users)" -p "User $USER for login to drive?: " usr
+	readyn -p "No password? (You will have to set it otherwise)" nopswd
+	if ! test "$usr" ; then
+	    usr=$USER
+	fi
+	
+	if [[ "$nopswd" == "y" ]]; then
+	    if ! sudo grep -q 'null passwords' /etc/samba/smb.conf; then
+	        sudo sed -i 's|\(####### Authentication #######\)|\1\n\nnull passwords = yes|g' /etc/samba/smb.conf
+	    fi
+	    sudo smbpasswd -na $usr
+	else
+	    sudo smbpasswd -a $usr
+	fi
+	sudo systemctl restart smbd.service
+	sudo systemctl status smbd.service
+	
+	sudo systemctl restart nmbd.service
+	sudo systemctl status nmbd.service
 fi
-
-if [[ "y" == $browse ]]; then
-    browse="yes"
-else
-    browse="no"
-fi
-
-if [[ "y" == $public ]]; then
-    public="yes"
-else
-    public="no"
-fi
-
-if [ -z $fmask ]; then
-    fmask=0777
-else
-    fmask=0777
-fi
-
-if [ -z $dmask ]; then
-    dmask=0777
-else
-    dmask=0777
-fi
-
-
-printf "\n[$drive]
-    Path=$mnt
-    Browseable=$browse
-    Writeable=$write
-    Public=$public
-    Create mask=$fmask
-    Directory mask=$dmask" | sudo tee -a /etc/samba/smb.conf &> /dev/null
-
-readyn -Y "YELLOW"  -p "Edit /etc/samba/smb.conf" edit
-if test "$edit" == "y"; then
-    sudo $EDITOR /etc/samba/smb.conf
-fi
-
-reade -Q "GREEN" -i "$USER $(users)" -p "User $USER for login to drive?: " usr
-readyn -p "No password? (You will have to set it otherwise)" nopswd
-if ! test "$usr" ; then
-    usr=$USER
-fi
-
-if [[ "$nopswd" == "y" ]]; then
-    if ! sudo grep -q 'null passwords' /etc/samba/smb.conf; then
-        sudo sed -i 's|\(####### Authentication #######\)|\1\n\nnull passwords = yes|g' /etc/samba/smb.conf
-    fi
-    sudo smbpasswd -na $usr
-else
-    sudo smbpasswd -a $usr
-fi
-sudo systemctl restart smbd.service
-sudo systemctl status smbd.service
-
-sudo systemctl restart nmbd.service
-sudo systemctl status nmbd.service
